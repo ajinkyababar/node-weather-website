277593cdd4eecd583704173686bf8de1532220eb
Babar, Ajinkya <ajinkya_babar_tp@bmc.com>
2020-05-26 18:09:35 +0530

    change request module to postman-request

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,4 @@
-const request = require('request')
+const request = require('postman-request')
 
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=3a1a9731c2f63669d32bcd076fd14456&query=' + latitude + ',' + longitude
